3cd8689c714020f2bdcabbbede510be4ff92a8f4
Isreal6ix <isrealsix@gmail.com>
2021-07-28 21:49:32 +0700

    mounted the header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Fragment } from 'react';
+import Header from './components/Layout/Header';
+
 import './App.css';
 
 function App() {
 	return (
-		<div>
-			<h2>Let's get started</h2>
-		</div>
+		<Fragment>
+			<Header />
+		</Fragment>
 	);
 }
 
