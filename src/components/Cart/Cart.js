05233c8d54c809744fd9752b5008441763353a1f
Isreal6ix <isrealsix@gmail.com>
2021-07-29 21:56:46 +0700

    removed a lot of bugs

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,9 +11,13 @@ const Cart = props => {
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 	const hasItems = cartCtx.items.length > 0;
 
-	const cartItemRemoveHandler = () => {};
+	const cartItemRemoveHandler = id => {
+		cartCtx.removeItem(id);
+	};
 
-	const cartItemAddHandler = () => {};
+	const cartItemAddHandler = item => {
+		cartCtx.addItem({ ...item, amount: 1 });
+	};
 
 	const cartItems = (
 		<ul className={classes['cart-items']}>
@@ -24,7 +28,7 @@ const Cart = props => {
 					amount={item.amount}
 					price={item.price}
 					onRemove={cartItemRemoveHandler.bind(null, item.id)}
-					onAdd={cartItemAddHandler.bind(item)}
+					onAdd={cartItemAddHandler.bind(null, item)}
 				/>
 			))}
 		</ul>
