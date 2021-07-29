9569cade541a0adb27541a0203ee2b4bea3c7a0c
Isreal6ix <isrealsix@gmail.com>
2021-07-29 20:05:30 +0700

    added comments

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -11,31 +11,42 @@ const cartReducer = (state, action) => {
 	if ((action.type = 'ADD')) {
 		const updatedTotalAmount =
 			state.totalAmount + action.item.price * action.item.amount;
+
+		// Searched for the index of incoming item
 		const existingCartItemIndex = state.items.findIndex(
 			item => item.id === action.item.id
 		);
 
+		// If it exists in state
 		const existingCartItem = state.items[existingCartItemIndex];
 
 		let updatedItem, updatedItems;
 
+		// if it exists, spread the state and only increment the amount
 		if (existingCartItem) {
 			updatedItem = {
 				...existingCartItem,
 				amount: existingCartItem.amount + action.item.amount,
 			};
 
+			// Spread the state data
 			updatedItems = [...state.items];
+
+			// locate and update the data via index to the updatedItem
 			updatedItems[existingCartItemIndex] = updatedItem;
 		} else {
+			// if new Item, populate the cart
 			updatedItems = state.items.concat(action.item);
 		}
 
+		// Return state
 		return {
 			items: updatedItems,
 			totalAmount: updatedTotalAmount,
 		};
 	}
+
+	// Return default state
 	return defaultCartState;
 };
 
