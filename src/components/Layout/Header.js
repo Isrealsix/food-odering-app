50dca4793f18195733162edd8ade226341d443ad
Isreal6ix <isrealsix@gmail.com>
2021-07-29 15:33:57 +0700

    passed handlers to set the state of backdrop to open and close

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
 		<React.Fragment>
 			<header className={classes.header}>
 				<h1>PlaceMeals</h1>
-				<HeaderCartButton />
+				<HeaderCartButton onClick={props.onShowCart} />
 			</header>
 
 			<div className={classes['main-image']}>
