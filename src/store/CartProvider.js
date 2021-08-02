c167d3ce9fa56e2eec2f15b2242b0997e01d6f62
Isreal6ix <isrealsix@gmail.com>
2021-08-02 17:07:24 +0700

    added functionalities to clearCart on dubmit, display sending and sent messages

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -72,6 +72,10 @@ const cartReducer = (state, action) => {
 		};
 	}
 
+	if (action.type === 'CLEAR') {
+		return defaultCartState;
+	}
+
 	// Return default state
 	return defaultCartState;
 };
@@ -88,11 +92,16 @@ const CartProvider = props => {
 	const removeItemFromCartHandler = id => {
 		dispatchCartAction({ type: 'REMOVE', id: id });
 	};
+
+	const clearCartHandler = () => {
+		dispatchCartAction({ type: 'CLEAR' });
+	};
 	const cartContext = {
 		items: cartState.items,
 		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItem: removeItemFromCartHandler,
+		clearCart: clearCartHandler,
 	};
 	return (
 		<CartContext.Provider value={cartContext}>
